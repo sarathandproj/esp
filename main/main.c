31e6cebac9e744e2ba8776675f387dd0f0ea1fc1
sarath <sarathandproj@gmail.com>
2021-05-29 21:53:23 +0530

    RTOS

--- main/main.c
+++ main/main.c
@@ -2,14 +2,31 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+void printONE(void *parameter) {
 
-void app_main(void)
-{
+    for(;;) {
+
+        printf("1\n");
+
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
+}
+
+void printHW(void *parameter) {
+
     int i = 0;
     while (1) {
         printf("[%d] Hello world!\n", i);
-        printf("something saved  creditinals\n");
+        printf("something saved creditinals\n");
         i++;
         vTaskDelay(5000 / portTICK_PERIOD_MS);
     }
+
+}
+
+
+void app_main(void)
+{
+    xTaskCreatePinnedToCore(printHW," print HW",2048,NULL,1,NULL,1);
+    
 }
